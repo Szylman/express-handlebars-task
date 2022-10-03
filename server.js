516baa9f8350468a2b1220acf534f023e83ff59c
Szylman96 <szymek2255@gmail.com>
2022-10-03 13:32:26 +0200

    create contatct.hbs

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
-const hbs = require('express-handlebars');
-
 const app = express();
+const hbs = require('express-handlebars');
+const multer = require("multer");
+const upload = multer({dest: 'public/image'})
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.urlencoded({ extended: false }));
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
@@ -36,6 +39,19 @@ app.get('/history', (req, res) => {
 app.get('/hello/:name', (req,res) =>{
     res.render('hello', {name:req.params.name});
 })
+
+app.post('/contact/send-message', upload.single('uploaded_file'), (req, res) => {
+
+    const { author, sender, title, message} = req.body;
+    const image = req.file;
+
+    if(author && sender && title && message && image) {
+        res.render('contact', { isSent: true,  name: req.file.filename});
+    }
+    else {
+        res.render('contact', { isError: true });
+    }
+});
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
